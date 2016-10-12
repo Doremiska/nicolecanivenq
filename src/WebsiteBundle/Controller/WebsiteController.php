591ed9559755dacd2ccfa0383a3649977d58afa4
Doremiska <doremiska@gmail.com>
2016-10-12 16:49:19 +0200

    Intégration d'un premier design + construction de la page d'accueil

---
+++ src/WebsiteBundle/Controller/WebsiteController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WebsiteController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('WebsiteBundle:Website:index.html.twig');
+    }
+    
+    public function sophrologieAction()
+    {
+        return $this->render('WebsiteBundle:Website:sophrologie.html.twig');
+    }
+    
+    public function soinsEnergetiquesAction()
+    {
+        return $this->render('WebsiteBundle:Website:soins_energetiques.html.twig');
+    }
+    
+    public function quiSuisJeAction()
+    {
+        return $this->render('WebsiteBundle:Website:qui_suis_je.html.twig');
+    }
+    
+    public function bibliographieAction()
+    {
+        return $this->render('WebsiteBundle:Website:bibliographie.html.twig');
+    }
+    
+    public function tarifsAction()
+    {
+        return $this->render('WebsiteBundle:Website:tarifs.html.twig');
+    }
+    
+    public function contactAction()
+    {
+        return $this->render('WebsiteBundle:Website:contact.html.twig');
+    }
+}
---
+++ web/js/main.js
@@ -0,0 +1,44 @@
+// Script Scroll Smooth
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 800);
+            return false;
+            }
+        }
+    });
+});
+
+// Script Side Menu
+function openNav() {
+    document.getElementById("side-menu").style.width = "280px";
+    document.getElementById("body-content").style.marginLeft = "280px";
+    document.getElementById("body-content").style.marginRight = "-280px";
+    document.getElementById("cover").style.zIndex = "1";
+    document.getElementById("cover").style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+}
+
+function closeNav() {
+    document.getElementById("side-menu").style.width = "0";
+    document.getElementById("body-content").style.marginLeft = "0";
+    document.getElementById("body-content").style.marginRight = "0";
+    document.getElementById("cover").style.zIndex = "-1";
+    document.getElementById("cover").style.backgroundColor = "transparent";
+}
+
+function expandNav(name) {
+    document.getElementById("side-menu-expand-".concat(name)).style.maxHeight = "200px";
+    document.getElementById("side-menu-glyphicon-right-".concat(name)).style.display = "none";
+    document.getElementById("side-menu-glyphicon-down-".concat(name)).style.display = "inline-block";
+}
+
+function unexpandNav(name) {
+    document.getElementById("side-menu-expand-".concat(name)).style.maxHeight = "0";
+    document.getElementById("side-menu-glyphicon-right-".concat(name)).style.display = "inline-block";
+    document.getElementById("side-menu-glyphicon-down-".concat(name)).style.display = "none";
+}
