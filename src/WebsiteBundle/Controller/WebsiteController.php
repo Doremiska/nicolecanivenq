ab0bff812c9f07eca2d494db19ab33ce90cd9f0d
Doremiska <doremiska@gmail.com>
2016-10-25 18:47:11 +0200

    Corrections email début intégration page de contact + intégration page de RDV avec nouveau bundle pour les input mask du téléphone

--- src/WebsiteBundle/Controller/WebsiteController.php
+++ src/WebsiteBundle/Controller/WebsiteController.php
@@ -5,6 +5,7 @@ namespace WebsiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use WebsiteBundle\Form\Type\ContactType;
+use WebsiteBundle\Form\Type\ContactRDVType;
 
 class WebsiteController extends Controller
 {
@@ -53,13 +54,18 @@ class WebsiteController extends Controller
         return $this->render('WebsiteBundle:Website:bibliographie.html.twig');
     }
     
+    public function biblioMediasAction()
+    {
+        return $this->render('WebsiteBundle:Website:biblio_medias.html.twig');
+    }
+    
     public function contactAction(Request $request)
     {
         $form = $this->createForm(ContactType::class);
         
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             $data = $form->getData();
-            $message = \Swift_Message::newInstance()->setSubject($data['subject'])->setFrom($data['email'])->setTo('doremiska@gmail.com')
+            $message = \Swift_Message::newInstance()->setSubject($data['subject'])->setFrom($data['email'])->setTo('nicole.canivenq@worldonline.fr')
                 ->setBody($this->renderView('Emails/email_contact.html.twig', array(
                         'firstName' => $data['firstName'],
                         'lastName' => $data['lastName'],
@@ -74,5 +80,30 @@ class WebsiteController extends Controller
         }
         
         return $this->render('WebsiteBundle:Website:contact.html.twig', array('form' => $form->createView()));
+    }
+    
+    public function contactRDVAction(Request $request)
+    {
+        $form = $this->createForm(ContactRDVType::class);
+        
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $data = $form->getData();
+            $message = \Swift_Message::newInstance()->setSubject("Demande de rendez-vous")->setFrom($data['email'])->setTo('nicole.canivenq@worldonline.fr')
+                ->setBody($this->renderView('Emails/email_contact_rdv.html.twig', array(
+                        'firstName' => $data['firstName'],
+                        'lastName' => $data['lastName'],
+                        'email' => $data['email'],
+                        'tel' => $data['tel'],
+                        'appointmentFor' => $data['appointmentFor'],
+                        'availableDay' => $data['availableDay'],
+                        'availableTime' => $data['availableTime'],
+                        'content' => $data['content']
+                )),'text/html');
+            $this->get('mailer')->send($message);
+            $request->getSession()->getFlashBag()->add('notice', "Votre demande de rendez-vous a bien été envoyée.");
+            return $this->redirect($this->generateUrl('website_contact_rdv').'#formulaire_rdv');
+        }
+        
+        return $this->render('WebsiteBundle:Website:contact_rdv.html.twig', array('form' => $form->createView()));
     }
 }
--- src/WebsiteBundle/Form/Type/ContactRDVType.php
+++ src/WebsiteBundle/Form/Type/ContactRDVType.php
@@ -8,12 +8,14 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\Regex;
 
-class ContactType extends AbstractType
+
+class ContactRDVType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
@@ -22,15 +24,15 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lastName', TextType::class, array(
-                'constraints' => array(
-                    new NotBlank(array('message' => 'Vous devez entrer votre prénom.')),
-                    new Length(array('max' => 255, 'maxMessage' => 'Ce champ ne peut pas excéder {{ limit }} caractères.'))
-                )
-            ))
             ->add('firstName', TextType::class, array(
                 'constraints' => array(
                     new NotBlank(array('message' => 'Vous devez entrer votre nom.')),
+                    new Length(array('max' => 255, 'maxMessage' => 'Ce champ ne peut pas excéder {{ limit }} caractères.'))
+                )
+            ))
+            ->add('lastName', TextType::class, array(
+                'constraints' => array(
+                    new NotBlank(array('message' => 'Vous devez entrer votre prénom.')),
                     new Length(array('max' => 255, 'maxMessage' => 'Ce champ ne peut pas excéder {{ limit }} caractères.'))
                 )
             ))
@@ -46,17 +48,39 @@ class ContactType extends AbstractType
                     new Regex(array('pattern' => '/^\d{2}[[:blank:]]\d{2}[[:blank:]]\d{2}[[:blank:]]\d{2}[[:blank:]]\d{2}$/', 'message' => 'Veuillez rentrer un numéro de téléphone valide.'))
                 )
             ))
-            ->add('subject', TextType::class, array(
+            ->add('appointmentFor', ChoiceType::class, array(
+                'choices'  => array(
+                    'Sophrologie' => 'Sophrologie',
+                    'Soins énergetiques' => 'Soins énergétiques'
+                ),
+                'expanded' => true,
+                'multiple' => true,
                 'constraints' => array(
-                    new NotBlank(array('message' => "Vous devez remplir l'objet de votre message.")),
+                    new NotBlank(array('message' => "Vous devez sélectionner au moins une activité.")),
+                )
+            ))
+            ->add('availableDay', ChoiceType::class, array(
+                'choices'  => array(
+                    'Lundi' => 'Lundi',
+                    'Mardi' => 'Mardi',
+                    'Mercredi' => 'Mercredi',
+                    'Jeudi' => 'Jeudi',
+                    'Vendredi' => 'Vendredi' 
+                ),
+                'expanded' => true,
+                'multiple' => true,
+                'constraints' => array(
+                    new NotBlank(array('message' => "Merci de sélectionner les jours où vous êtes disponibles.")),
+                )
+            ))
+            ->add('availableTime', TextType::class, array(
+                'constraints' => array(
+                    new NotBlank(array('message' => "Merci d'indiquer les horaires auxquels vous êtes disponibles.")),
                     new Length(array('max' => 255, 'maxMessage' => 'Ce champ ne peut pas excéder {{ limit }} caractères.'))
                 )
             ))
             ->add('content', TextareaType::class, array(
-                'constraints' => array(
-                    new NotBlank(array('message' => 'Vous devez remplir le contenu de votre message.')),
-                    new Length(array('min' => 10, 'minMessage' => 'Votre message est trop court.'))
-                )
+                'required' => false
             ))
             ->add('envoyer', SubmitType::class)
         ;
